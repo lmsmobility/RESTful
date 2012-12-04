9bab7dd5ff9dc6919145f0ec6ccd599279e11e01
Rajiv Ghangrekar <rajiv.ghangrekar@agilysys.com>
2012-12-04 10:20:42 -0500

    commit on 12/03

--- lmsmobile/lmsmobile/src/main/java/org/agl/lmsmobile/controller/DemoController.java
+++ lmsmobile/lmsmobile/src/main/java/org/agl/lmsmobile/controller/DemoController.java
@@ -5,13 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/testMobile")
-public class TestController {
+@RequestMapping("/demoMobile")
+public class DemoController {
 	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String testAlive(@PathVariable String name) {
+	public @ResponseBody String testAlive(@PathVariable String name) {
  
 		System.out.println("Yes I am alive " + name);
 		return "Hello " + name;
